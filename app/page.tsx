cb3e94e2293ba8fd547e29d1696b1d3be24932ac
Leonid Meleshin <hello@leon0399.ru>
2024-10-29 16:35:06 +0100

    ✨ display table of stolen stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import { parseMarkdownTable } from '@/lib/markdown';
 import { promises as fs } from 'fs';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import videosData from '../data/videos.json';
 
 export type StolenProperty = {
   /**
@@ -20,6 +29,15 @@ export type StolenProperty = {
   Justification: string;
 }
 
+export type Video = {
+  Title: string;
+  Series: string;
+}
+
+export type Videos = Record<string, Video>;
+
+const videos = videosData as Videos;
+
 export default async function Home() {
   const markdownContent = await fs.readFile(process.cwd() + '/STOLEN.md', 'utf8');
   const data = parseMarkdownTable(markdownContent) as StolenProperty[];
@@ -27,9 +45,32 @@ export default async function Home() {
   return (
     <div className="">
       <main className="">
-        <pre>
-          {JSON.stringify(data, null, 2)}
-        </pre>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Video</TableHead>
+              <TableHead className="text-right">Timecode</TableHead>
+              <TableHead>Who</TableHead>
+              <TableHead>Product</TableHead>
+              <TableHead className="text-right">Estimated Price</TableHead>
+              <TableHead>Justification</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((row, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <a href={row.Video} target="_blank" rel="noreferrer">{videos[row.Video]?.Title ?? row.Video}</a>
+                </TableCell>
+                <TableCell className="text-right">{row.Timecode}</TableCell>
+                <TableCell>{row.Who}</TableCell>
+                <TableCell>{row.Product}</TableCell>
+                <TableCell className="text-right">{row["Est. price"]}</TableCell>
+                <TableCell>{row.Justification}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </main>
     </div>
   );
